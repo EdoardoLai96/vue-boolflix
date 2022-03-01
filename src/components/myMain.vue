b15c69c9acf71f87bce45544629cd63738b2de2b
Edoardo Lai <edoardolai96@gmail.com>
2022-03-01 23:16:16 +0100

    esercizio terminato fix separazione sezioni

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="container">
+    <h1>MOVIES</h1>
     <div class="movie_box">
         <myMovieCard :movie="movie" v-for="(movie, index) in moviesCatalogue" :key="'movie-'+ index" />
-      
+    </div>
+    <h1>TV SERIES</h1>
+    <div class="movie_box">
         <myTvShowCard :tvShow="tvShow" v-for="(tvShow, index) in tvShowsCatalogue" :key="'tvShow-'+ index" />
     </div>
 </div>
@@ -45,7 +48,12 @@ export default {
     .fa-star{
         color: rgb(255, 230, 0);
     }
-
+    h1{
+        margin-bottom: 4rem;
+        color: white;
+        font-size: 3rem;
+        font-family: Arial, Helvetica, sans-serif;
+    }
     .container{
         background-color: rgb(31, 31, 31);
         min-height: calc(100vh - 100px);
@@ -53,7 +61,8 @@ export default {
         .movie_box{
             display: flex;
             flex-wrap: wrap;
-            justify-content: center;
+            justify-content: flex-start;
+            margin-bottom: 3rem;
         }
     }
 </style>
