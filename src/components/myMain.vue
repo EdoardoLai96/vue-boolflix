44f54be1d8eb3f8d7c4e10c238b40770fb56e697
Edoardo Lai <edoardolai96@gmail.com>
2022-05-02 15:45:02 +0200

    Aggiunto search main

--- src/components/myMain.vue
+++ src/components/myMain.vue
@@ -1,10 +1,14 @@
 <template>
-<div class="container">
-    <h1>MOVIES</h1>
+<div class="container" id="main_container">
+    <div id="input_search_box">
+        <h1>Type a Movie or a Tv Show title</h1>
+        <input v-model="searchedTextMain" @keypress.enter="searchMain()" type="text" >
+    </div>
+    <h1 id="movies_headline"></h1>
     <div class="movie_box">
         <myMovieCard :movie="movie" v-for="(movie, index) in moviesCatalogue" :key="'movie-'+ index" />
     </div>
-    <h1>TV SERIES</h1>
+    <h1 id="tv_shows_headline"></h1>
     <div class="movie_box">
         <myTvShowCard :tvShow="tvShow" v-for="(tvShow, index) in tvShowsCatalogue" :key="'tvShow-'+ index" />
     </div>
@@ -18,7 +22,8 @@ export default {
     name: "myMain",
     data(){
         return{
-         info : []
+         info : [],
+         searchedTextMain : ''
         }
     },
     props: {
@@ -40,7 +45,21 @@ export default {
                 return Math.ceil(element.vote_average / 2)
             }); 
         },
-    }
+    },
+    methods:{
+        searchMain(){
+            this.$emit("searchMovieMain", this.searchedTextMain),
+            this.$emit("searchTvShowMain", this.searchedTextMain)
+            if(document.getElementById("input_search_box")){
+            document.getElementById("input_search_box").remove();
+            document.getElementById("movies_headline").innerHTML = "MOVIES",
+            document.getElementById("tv_shows_headline").innerHTML = "TV SHOWS"
+            document.getElementById("main_container").style.display = "block"
+
+        }
+
+        }
+    },
 }
 </script>
 
@@ -53,16 +72,43 @@ export default {
         color: white;
         font-size: 3rem;
         font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
     }
     .container{
         background-color: rgb(31, 31, 31);
         min-height: calc(100vh - 100px);
         padding: 4rem;
+        transition: 5s;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        #input_search_box{
+            flex-direction: row;
+            input{
+               width: 100%;
+                height: 50px;
+                font-size: 2rem;
+            }
+        }
         .movie_box{
             display: flex;
             flex-wrap: wrap;
-            justify-content: flex-start;
+            justify-content: center;
             margin-bottom: 3rem;
+            justify-items: flex-start;
         }
+        
+
     }
+
+        .animation{
+         animation-name: example;
+         animation-duration: 4s;
+
+        } 
+
+        @keyframes example {
+         from {background-color: red;}
+         to {background-color: yellow;}
+        }
 </style>
